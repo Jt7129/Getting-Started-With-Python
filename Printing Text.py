acd43b5747f1e588375c9b4cca4338374827c683
Jt7129 <116584013+Jt7129@users.noreply.github.com>
2023-02-25 17:00:27 -0500

    Create Printing Text.py

---
+++ Printing Text.py
@@ -0,0 +1,6 @@
+#This is a comment in python and this program when ran will print Hello World and then quit
+print("Hello World")
+#You can replace the text in the "" to change what the program prints 
+#you can duplicate this line code to print multiple Line or use this line of code
+print("Hello\nWorld")
+#this will print Hello on one line and World on another
