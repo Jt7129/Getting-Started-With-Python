fdd4a043af79123223c8bb189275fa6017512009
Jt7129 <116584013+Jt7129@users.noreply.github.com>
2023-02-25 17:36:37 -0500

    Create Values and String variables.py

---
+++ Values and String variables.py
@@ -0,0 +1,14 @@
+#Lets make a String var and a value var
+Stringvar = "String Variable"
+
+Valuevar = 6
+Valuevar2 = 4
+
+#This will print out String Variable
+
+print(Stringvar)
+
+#This will add our value variables
+print(Valuevar + Valuevar2)
+#They add become they are values not strings
+#strings are text Values
